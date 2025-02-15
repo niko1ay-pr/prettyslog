1dcd46bab1c7a438b4a4df706fd89560045987b2
Nikolay Prikhodko <niko1ay.pr@ya.ru>
2025-02-15 20:13:34 +0300

    Final implementation

--- prettyslog.go
+++ prettyslog.go
@@ -3,6 +3,7 @@ package prettyslog
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log/slog"
 	"sync"
@@ -49,6 +50,7 @@ func (h *Handler) WithGroup(name string) slog.Handler {
 }
 
 func (h *Handler) Handle(ctx context.Context, r slog.Record) error {
+	op := "handler.Handle"
 	level := r.Level.String() + ":"
 
 	switch r.Level {
@@ -62,11 +64,79 @@ func (h *Handler) Handle(ctx context.Context, r slog.Record) error {
 		level = colorize(lightYellow, level)
 	}
 
-	fmt.Println(colorize(lightGray, r.Time.Format(timeFormat)), level, colorize(white, r.Message))
+	attr, err := h.extractAttrs(ctx, r)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	bytes, err := json.MarshalIndent(attr, "", "  ")
+
+	fmt.Println(colorize(lightGray, r.Time.Format(timeFormat)),
+		level,
+		colorize(white, r.Message),
+		colorize(darkGray, string(bytes)),
+	)
 
 	return nil
 }
 
+func NewHandler(opts *slog.HandlerOptions) *Handler {
+	if opts == nil {
+		opts = &slog.HandlerOptions{}
+	}
+
+	b := &bytes.Buffer{}
+	return &Handler{
+		b: b,
+		h: slog.NewJSONHandler(b, &slog.HandlerOptions{
+			Level:       opts.Level,
+			AddSource:   opts.AddSource,
+			ReplaceAttr: supressDefaults(opts.ReplaceAttr),
+		}),
+		m: &sync.Mutex{},
+	}
+}
+
 func colorize(colorCode int, v string) string {
 	return fmt.Sprintf("\033[%sm%s%s", colorCode, v, reset)
+}
+
+func (h *Handler) extractAttrs(ctx context.Context, r slog.Record) (map[string]any, error) {
+	op := "hanler.extractAttrs"
+	h.m.Lock()
+
+	defer func() {
+		h.b.Reset()
+		h.m.Unlock()
+	}()
+
+	err := h.h.Handle(ctx, r)
+
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	var attrs map[string]any
+	err = json.Unmarshal(h.b.Bytes(), &attrs)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return attrs, nil
+}
+
+func supressDefaults(next func([]string, slog.Attr) slog.Attr) func([]string, slog.Attr) slog.Attr {
+	return func(groups []string, attr slog.Attr) slog.Attr {
+		if attr.Key == slog.TimeKey ||
+			attr.Key == slog.LevelKey ||
+			attr.Key == slog.MessageKey {
+			return slog.Attr{}
+		}
+		if next == nil {
+			return attr
+		}
+
+		return next(groups, attr)
+
+	}
 }
